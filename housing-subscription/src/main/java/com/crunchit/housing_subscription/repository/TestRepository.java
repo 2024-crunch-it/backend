efa1fe08ac1b5edec1b5fd13df95cae55ad9a218
Wonyoung Cheong <86768006+cheongwonyoung@users.noreply.github.com>
2024-11-18 12:20:51 +0900

    remove: test, redis 초기 코드 삭제

--- housing-subscription/src/main/java/com/crunchit/housing_subscription/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crunchit.housing_subscription.repository;
-
-import com.crunchit.housing_subscription.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
